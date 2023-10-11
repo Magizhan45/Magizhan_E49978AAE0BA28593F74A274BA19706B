5ee8777e8510447a245393f9f0d6ee8032dfc204
Magizhan_Starboy <144884273+Magizhan45@users.noreply.github.com>
2023-10-11 11:52:03 +0530

    Add files via upload

---
+++ Leap_year.py
@@ -0,0 +1,14 @@
+def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+# Test the function
+print(is_leap_year(2024))
